016ccee83493e544090bdba297c7f14591f69a3d
Pankaj Jha <pankajjhawork@gmail.com>
2024-12-12 21:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeColor() {
+  const colorSelect = document.getElementById("colorSelect");
+  
+  // Check if an option is selected
+  if (colorSelect.selectedIndex !== -1) {
+    // Remove the selected option
+    colorSelect.remove(colorSelect.selectedIndex);
+  }
+}
